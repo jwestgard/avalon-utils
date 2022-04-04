210a34bc31890512f6383162733ac3c57a859a4e
Peter Eichman <peichman@umd.edu>
2022-04-04 13:36:57 -0400

    LIBAVALON-233. Updates to add_file_paths.py script.
    - Read CSV from STDIN and write extended CSV to STDOUT
    - Created an insert_next_file() function to make the filename update
      process clearer
    - Added a python3 shebang line
    - Use collections.defaultdict instead of calling setdefault() for each
      key in the results dictionary
    - Create a lookup list of column indexes for the "Other Identifier"
      columns
    - Created find_umdm() function
    - Added a usage message when run with no arguments
    
    https://issues.umd.edu/browse?LIBAVALON-233

--- add_file_paths.py
+++ add_file_paths.py
@@ -1,14 +1,47 @@
+#!/usr/bin/env python3
+
 import csv
 import re
 import sys
+from collections import defaultdict
+
 
 PATTERN = r'^.+?/(umd_\d+)/(umd_\d+)/(.*)$'
 CAMPUSFLAG = "Access is restricted to patrons at the University of Maryland."
 
-results = {}
+results = defaultdict(list)
 
-with open(sys.argv[1]) as handle:
-    data = [row.strip() for row in handle.readlines()]
+
+def find_umdm(other_identifier_columns, row):
+    for i in other_identifier_columns:
+        if row[i].startswith('umd'):
+            return row[i]
+
+
+def insert_next_file(header, row, start, filename, pid, path):
+    """
+    Find the next column with a "File" header that occurs after the column
+    numbered start. Add the path and pid to the appropriate columns of the row,
+    and return the column number of the "File" column just updated.
+    """
+    i = header.index("File", start)
+    print(f"{row[i]} => {filename}", file=sys.stderr)
+    row[i] = path
+    row[i+1] = pid
+    return i
+
+
+try:
+    with open(sys.argv[1]) as handle:
+        data = [row.strip() for row in handle.readlines()]
+except IndexError:
+    print(
+        'Usage: add_file_paths.py <file_list.txt>\n'
+        '\n'
+        'Reads the input CSV from STDIN and writes the output CSV to STDOUT\n',
+        file=sys.stderr
+    )
+    sys.exit()
 
 for n, path in enumerate(data, 1):
     match = re.match(PATTERN, path)
@@ -18,7 +51,7 @@ for n, path in enumerate(data, 1):
         umam = match.group(2).replace("_", ":")
         filename = match.group(3)
         path = f"{match.group(1)}/{match.group(2)}/{filename}"
-        results.setdefault(umdm, []).append((filename, umam, path))
+        results[umdm].append((filename, umam, path))
 
 """
 for k, vals in results.items():
@@ -27,39 +60,30 @@ for k, vals in results.items():
         print(f"  ({n}) {v}")
 """
 
-with open(sys.argv[2]) as inputhandle, open(sys.argv[3], 'w') as outputhandle:
-    reader = csv.reader(inputhandle)
-    header = next(reader)
-    header.extend(['Note Type', 'Note'])
-    # print(len(header))
-    writer = csv.writer(outputhandle)
-    writer.writerow(header)
-    for n, row in enumerate(reader, 1):
+# read the input CSV from STDIN
+reader = csv.reader(sys.stdin)
+header = next(reader)
+header.extend(['Note Type', 'Note'])
+other_identifier_columns = [i for i, h in enumerate(header) if h == 'Other Identifier']
 
-        # Set access note field
-        if row[header.index('Terms of Use')] == CAMPUSFLAG:
-            row.extend(['access', 'campus-only'])
-        else:
-            row.extend(['access', 'public'])
-        
-        # Lookup files using UMDM PID and populate columns
-        for i, col in enumerate(row):
-            if header[i] == "Other Identifier" and row[i].startswith('umd'):
-                umdm = row[i]
-            else:
-                continue
-        if not umdm:
-            sys.exit(f"Could not locate UMDM identifier for row {n}")
-        files = results[umdm]
-        start = 0
-        while sorted(files):
-            i = header.index("File", start)
-            (filename, pid, path) = files.pop(0)
-            print(f"{row[i]} => {filename}")
-            row[i] = path
-            row[i+1] = pid
-            start = i
-            
-            
-        
-        writer.writerow(row)
+# write the output CSV to STDOUT
+writer = csv.writer(sys.stdout)
+writer.writerow(header)
+for n, row in enumerate(reader, 1):
+
+    # Set access note field
+    if row[header.index('Terms of Use')] == CAMPUSFLAG:
+        row.extend(['access', 'campus-only'])
+    else:
+        row.extend(['access', 'public'])
+
+    # Lookup files using UMDM PID and populate columns
+    umdm = find_umdm(other_identifier_columns, row)
+    if not umdm:
+        sys.exit(f"Could not locate UMDM identifier for row {n}")
+    files = results[umdm]
+    start = 0
+    for filename, pid, path in sorted(files):
+        start = insert_next_file(header, row, start, filename, pid, path)
+
+    writer.writerow(row)
