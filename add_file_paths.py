c89e446081f2104142e5ea2e4e6fe33438ed5568
Joshua Westgard <jwestgard@mac.com>
2022-04-22 15:04:30 -0400

    move update details into config file

--- add_file_paths.py
+++ add_file_paths.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
 
 import csv
+import os
 import re
 import sys
+import yaml
 from collections import defaultdict
 
 
-PATTERN = r'^.+?/(umd_\d+)/(umd_\d+)/(.*)$'
-CAMPUSFLAG = "Access is restricted to patrons at the University of Maryland."
-
 results = defaultdict(list)
+
+
+with open('config.yaml') as handle:
+    config = yaml.safe_load(handle)
 
 
 def find_umdm(other_identifier_columns, row):
@@ -27,7 +30,7 @@ def insert_next_file(header, row, n, start, pid, path):
     i = header.index("File", start)
     print(f"Updating row {n}, col {i}: {row[i]} => {path}", file=sys.stderr)
     row[i] = path
-    row[i+1] = pid
+    row[i+1] = os.path.basename(path)
     return i + 1
 
 
@@ -45,19 +48,22 @@ def add_file_paths():
         sys.exit()
 
     for n, path in enumerate(data, 1):
-        match = re.match(PATTERN, path)
+        pattern = r'{}'.format(config.get('path_pattern'))
+        match = re.match(pattern, path)
         if match:
             umdm = match.group(1).replace("_", ":")
             umam = match.group(2).replace("_", ":")
             filename = match.group(3)
-            path = f"{match.group(1)}/{match.group(2)}/{filename}"
+            path = f'{match.group(1)}/{match.group(2)}/{filename}'
             results[umdm].append((filename, umam, path))
 
     # read the input CSV from STDIN
     reader = csv.reader(sys.stdin)
     header = next(reader)
-    header.extend(['Note Type', 'Note'])
-    other_identifier_columns = [i for i, h in enumerate(header) if h == 'Other Identifier']
+    header.extend(['Note Type', 'Note', 'Offset'])
+    other_identifier_columns = [
+        i for i, h in enumerate(header) if h == 'Other Identifier'
+        ]
 
     # write the output CSV to STDOUT
     writer = csv.writer(sys.stdout)
@@ -66,10 +72,16 @@ def add_file_paths():
     for n, row in enumerate(reader, 2):
 
         # Set access note field
-        if row[header.index('Terms of Use')] == CAMPUSFLAG:
-            row.extend(['access', 'campus-only'])
+        if row[header.index('Terms of Use')] == config.get('campus_flag'):
+            row.extend(['access', config.get('access_campus')])
         else:
-            row.extend(['access', 'public'])
+            row.extend(['access', config.get('access_public')])
+
+        # If offset found in config, set it, otherwise leave blank
+        if 'offset' in config:
+            row.append(config.get('offset'))
+        else:
+            row.append('')
 
         # Lookup files using UMDM PID and populate columns
         umdm = find_umdm(other_identifier_columns, row)
@@ -77,6 +89,17 @@ def add_file_paths():
             sys.exit(f"Could not locate UMDM identifier for row {n}")
         files = results[umdm]
         files.sort()
+
+        # Update PID identifier type
+        row[2] = 'fedora'
+
+        # Update filename identifier
+        if row[6] == 'local':
+            row[6] = 'filename'
+        elif row[6] == '' and len(files) > 0:
+            row[6] = 'filename'
+            row[7] = os.path.splitext(files[0][0])[0]
+
         start = 0
         for filename, pid, path in files:
             start = insert_next_file(header, row, n, start, pid, path)
