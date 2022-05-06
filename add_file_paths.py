4ac7b355fd0b2dd234027a7b617f53fe24644083
Joshua Westgard <jwestgard@mac.com>
2022-05-06 18:10:08 -0400

    add file number and size stats counter

--- add_file_paths.py
+++ add_file_paths.py
@@ -38,7 +38,8 @@ def insert_next_file(header, row, n, start, pid, path):
 def add_file_paths():
     try:
         with open(sys.argv[1]) as input_file:
-            data = [row.strip() for row in input_file.readlines()]
+            lines = [row.strip() for row in input_file.readlines()]
+            data = [tuple(line.split(',')) for line in lines]
     except IndexError:
         print(
             'Usage: add_file_paths.py <file_list.txt>\n'
@@ -48,16 +49,16 @@ def add_file_paths():
         )
         sys.exit()
 
-    for n, path in enumerate(data, 1):
-        path_prefix = config.get('binaries_location', '')
-        pattern = r'{}'.format(config.get('path_pattern'))
+    path_prefix = config.get('path_prefix', '')
+    pattern = r'{}'.format(config.get('path_pattern'))
+    for n, (pid, path, bytes) in enumerate(data, 1):
         match = re.match(pattern, path)
         if match:
             umdm = match.group(1).replace("_", ":")
             umam = match.group(2).replace("_", ":")
             filename = match.group(3)
             path = f'{path_prefix}/{match.group(1)}/{match.group(2)}/{filename}'
-            results[umdm].append((filename, umam, path))
+            results[umdm].append((filename, umam, path, int(bytes)))
 
     # read the input CSV from STDIN
     reader = csv.reader(sys.stdin)
@@ -67,6 +68,11 @@ def add_file_paths():
         i for i, h in enumerate(header) if h == 'Other Identifier'
         ]
     identifier_patterns = [(i['pattern'], i['label']) for i in config.get('id_mappings')]
+
+    # set up counters
+    items_added = 0
+    files_added = 0
+    bytes_added = 0
 
     # write the output CSV to STDOUT
     writer = csv.writer(sys.stdout)
@@ -105,14 +111,24 @@ def add_file_paths():
 
 
         start = 0
-        for filename, pid, path in files:
+        for filename, pid, path, bytes in files:
             start = insert_next_file(header, row, n, start, pid, path)
+            files_added += 1
+            bytes_added += bytes
 
-        all_filenames = [filename[:-4] for (filename, pid, path) in files]
+        all_filenames = [
+            filename[:-4] for (filename, pid, path, bytes) in files
+            ]
         print(f" All Files: {all_filenames}", file=sys.stderr)
-
+        items_added += 1
         writer.writerow(row)
 
+    print(f"\nBATCH COMPLETE", file=sys.stderr)
+    gb = round(bytes_added / 2**30, 2)
+    size_str = f"{bytes_added} bytes ({gb} GB)"
+    print(f"{items_added} items, {files_added} files, {size_str}",
+         file=sys.stderr
+         )
 
 if __name__ == '__main__':
     add_file_paths()
